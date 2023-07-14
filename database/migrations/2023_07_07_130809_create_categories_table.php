954c55a2d601fcfbd62d8370b02ee0486014dfc5
akiraoka29 <akiraoka.job@gmail.com>
2023-07-14 09:53:03 +0700

    Update Migration

--- database/migrations/2023_07_07_130809_create_categories_table.php
+++ database/migrations/2023_07_07_130809_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->id('category_id');
             $table->string('category_name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2023_07_07_130810_create_products_table.php
+++ database/migrations/2023_07_07_130810_create_products_table.php
@@ -19,7 +19,10 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger('category_id');
             $table->decimal('price', 10, 2);
             $table->text('description');
+            $table->string('file_name', 45);
+            $table->string('file_path');
             $table->timestamps();
+            $table->softDeletes();
 
             $table->foreign('category_id')->references('category_id')->on('categories');
         });
