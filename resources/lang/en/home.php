d1be1e3677a6c1304ea3d98bf7fa06cb1f318c93
akiraoka29 <akiraoka.job@gmail.com>
2023-07-19 16:45:50 +0700

    Add Landing Page Language for ID & EN

---
+++ resources/lang/en/home.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'home' => 'Home',
+    'contact_us' => 'Contact Us',
+    'all_category' => 'All Category',
+    'category' => 'Category',
+    'search_bar' => 'Search Products Here',
+    'hot_product' => 'Hot Products',
+    'login' => 'Login',
+    'my_account' => 'My Account',
+];
---
+++ resources/lang/id/home.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'home' => 'Beranda',
+    'contact_us' => 'Hubungi Kami',
+    'all_category' => 'Semua Kategori',
+    'category' => 'Kategori',
+    'search_bar' => 'Cari Produk Terbaru',
+    'hot_product' => 'Paling Dicari',
+    'login' => 'Masuk',
+    'my_account' => 'Akun Saya',
+];
