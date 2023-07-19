ee4561b50ab966d23f91ddad23318e8408400ef5
akiraoka29 <akiraoka.job@gmail.com>
2023-07-19 16:30:36 +0700

    Update Product Pivot Migration

--- database/migrations/2023_07_07_130810_create_products_table.php
+++ database/migrations/2023_07_07_130810_create_products_table.php
@@ -16,15 +16,12 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id('product_id');
             $table->string('product_name');
-            $table->unsignedBigInteger('category_id');
             $table->decimal('price', 10, 2);
             $table->text('description');
             $table->string('file_name', 45);
             $table->string('file_path');
             $table->timestamps();
             $table->softDeletes();
-
-            $table->foreign('category_id')->references('category_id')->on('categories');
         });
     }
 
