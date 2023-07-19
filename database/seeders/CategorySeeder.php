a3fedc1d55072dccb9292c38eaf4fd4eb30e88cf
akiraoka29 <akiraoka.job@gmail.com>
2023-07-19 21:19:25 +0700

    Update Category Migration & Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,14 +16,14 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $categories = [
-            ['category_name' => 'Smartphones'],
-            ['category_name' => 'Laptops'],
-            ['category_name' => 'Tablets'],
-            ['category_name' => 'TV'],
-            ['category_name' => 'Kamera'],
-            ['category_name' => 'Headphones'],
-            ['category_name' => 'Speakers'],
-            ['category_name' => 'Perabotan Rumah'],
+            ['category_name' => 'Smartphones', 'slug' => 'smartphones'],
+            ['category_name' => 'Laptops', 'slug' => 'laptops'],
+            ['category_name' => 'Tablets', 'slug' => 'tablets'],
+            ['category_name' => 'TV', 'slug' => 'tv'],
+            ['category_name' => 'Kamera', 'slug' => 'kamera'],
+            ['category_name' => 'Headphones', 'slug' => 'headphones'],
+            ['category_name' => 'Speakers', 'slug' => 'speakers'],
+            ['category_name' => 'Perabotan Rumah', 'slug' => 'perabotan-rumah'],
         ];
 
         Category::insert($categories);
